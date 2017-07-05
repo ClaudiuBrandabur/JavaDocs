42120d8b57eb95d65ff80ed62d3b87e172c3e21d
Claudiu Brandabur <claudiu.brandabur@gmail.com>
2017-07-05 17:44:24 +0300

    Part2

--- Collections/warmup-part1/src/main/java/exercise/exercise3/Exercise3.java
+++ Collections/warmup-part1/src/main/java/exercise/exercise3/Exercise3.java
@@ -1,6 +1,6 @@
 package exercise.exercise3;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Radu.Hoaghe on 04/20/2015.
@@ -35,16 +35,50 @@ public class Exercise3 {
         // TODO Exercise #3 b) add the elements from listToAdd to the Sets
 
         // TODO Exercise #3 c) Check the content of the Sets
-        System.out.println("\nThe elements contained in the first Set: ");
 
-        System.out.println("\nThe elements contained in the second Set: ");
+        Set<String> firstSet = new HashSet<String>();
 
-        System.out.println("\nThe elements contained in the third Set: ");
+        for (String i:listToAdd) {
+            firstSet.add(i);
+        }
 
+        System.out.println("\nThe elements contained in the first Set(HashSet): ");
+        System.out.println(firstSet);
 
-        System.out.println("\nThe elements contained in the TreeSet after inserting two duplicates: ");
+        Set<String> secondSet = new TreeSet<String>();
+
+        for (String i:listToAdd) {
+            secondSet.add(i);
+        }
+
+        System.out.println("\nThe elements contained in the second Set(TreeSet): ");
+        System.out.println(secondSet);
+
+        Set<String> thirdSet = new LinkedHashSet<String>();
+
+        for (String i:listToAdd) {
+            thirdSet.add(i);
+        }
+
+        System.out.println("\nThe elements contained in the third Set(LinkedHashSet): ");
+        System.out.println(thirdSet);
+
+        //System.out.println("\nThe elements contained in the TreeSet after inserting two duplicates: ");
 
         // TODO Exercise #3 d) Add to the TreeSet two elements that already exist in the Set
         // TODO Exercise #3 d) and print again the TreeSet. What do you see?
+
+        firstSet.add("A");
+        firstSet.add("no");
+        secondSet.add("A");
+        secondSet.add("no");
+        thirdSet.add("A");
+        thirdSet.add("no");
+        System.out.println("\nThe elements contained in the HashSet after inserting two duplicates: ");
+        System.out.println(firstSet);
+        System.out.println("\nThe elements contained in the TreeSet after inserting two duplicates: ");
+        System.out.println(secondSet);
+        System.out.println("\nThe elements contained in the LinkedHashSet after inserting two duplicates: ");
+        System.out.println(thirdSet);
     }
 }
